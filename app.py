6738578940fe0282621ee41f93f0c392aa9fa502
DevWizardMarcos <mp718887@gmail.com>
2026-04-22 16:54:31 -0300

    criando o Query Param

--- app.py
+++ app.py
@@ -21,8 +21,13 @@ itens = [
 # criado a rota de itens
 @app.get("/itens")
 
-def listar_itens():
-    return itens
+def listar_itens(nome = None): # Query Param
+    if nome: #filtrando por nome (se passou por exemplo ?nome=Cafe na URL)
+        for item in itens: 
+            if item['nome'] == nome: 
+                return item
+    else:
+        return itens #se nao passou nada
 
 #criando a a rota POST 
 
@@ -33,3 +38,13 @@ def criarItens(item:dict):
     item['id'] = novoId # colocando um novo id 
     itens.append(item) # adicionando na lista 
     return item
+
+#criando a rota para pegar o id 1 
+
+@app.get("/itens/{id}") # usando PATH Param um valor que faz parte do caminho da URL 
+
+def percorrer(id:  int): #parementro de tipo int que é o id
+    for i in itens: 
+        if i ['id'] == id: # se a chave i for == ao id 
+            return i # retorna o chave e o valor 
+        
