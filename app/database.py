406aeed615f87f613767e4704be14b145ab0942b
DevWizardMarcos <mp718887@gmail.com>
2026-05-05 22:19:27 -0300

    colocando a session

--- app/database.py
+++ app/database.py
@@ -1,5 +1,9 @@
 from sqlalchemy import create_engine 
+from sqlalchemy import sessionmaker
 
 #criando a conexao com o banco de dados 
-
 egine = create_engine('sqlite:///./banco.db')
+
+#criando a fabrica de session (a conversa com o banco de dados)
+sessionLocal = sessionmaker(bind = egine)
+                            #entrada especifica entrando no egine 
