4e93043939cfc64b880a062679303efcd7bf1e3f
DevWizardMarcos <mp718887@gmail.com>
2026-05-05 22:35:55 -0300

    colocando o model User

--- app/database.py
+++ app/database.py
@@ -1,9 +1,19 @@
-from sqlalchemy import create_engine 
-from sqlalchemy import sessionmaker
+
+from sqlalchemy import create_engine, Column, Integer, String 
+from sqlalchemy.orm import sessionmaker, declarative_base
+
 
 #criando a conexao com o banco de dados 
-egine = create_engine('sqlite:///./banco.db')
+engine = create_engine('sqlite:///./banco.db')
 
 #criando a fabrica de session (a conversa com o banco de dados)
-sessionLocal = sessionmaker(bind = egine)
+SessionLocal  = sessionmaker(bind = engine)
                             #entrada especifica entrando no egine 
+
+Base = declarative_base()
+
+#Criando uma classe que base registra models como tabelas do banco de dados
+class User(Base):
+    __tablename__ = 'users'
+    id = Column(Integer, primary_key = True)
+    nome = Column(String)
